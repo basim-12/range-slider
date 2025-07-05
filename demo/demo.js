bfd6ac7609d9f58d0519a391a2f98394a4127aa5
basim-12 <f223684@cfd.nu.edu.pk>
2025-07-06 00:32:34 +0500

    add custom bar

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,21 @@
-const myComponent = require('../src/index.js')
+const rangeSlider = require('../src/index.js')
 
-const x = myComponent()
+const range = rangeSlider()
 
-document.body.append(x)
+document.body.innerHTML = `
+    <h1> Range Slider </h1>
+`
+
+const main = document.createElement('div')
+main.classList.add('demo')
+
+const style = document.createElement('style')
+style.textContent = `
+    .demo {
+        border: 1px solid red;
+        padding: 50px;
+    }
+`
+main.append(style, range)
+
+document.body.append(main)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-module.exports = myComponent
+module.exports = rangeSlider
 
-function myComponent() {
+function rangeSlider() {
     const el = document.createElement('div') 
     el.classList.add('container')
 
@@ -9,21 +9,90 @@ function myComponent() {
     input = document.createElement('input')
     input.type = 'range'
 
+    bar = document.createElement('div')
+    bar.classList.add('bar')
+
+    
+    ruler = document.createElement('div')
+    ruler.classList.add('ruler')
+
+    fill = document.createElement('div')
+    fill.classList.add('fill')
+
+    bar.append(ruler, fill)
+
     style = document.createElement('style')
     style.textContent = get_theme()
 
-    shadow.append(style, input)
+    shadow.append(style, input, bar)
     return el
 }
 
 function get_theme() {
     return `
-        :host(.container) {
-            background-color: green;
-        }
+     :host { box-sizing: border-box; }
+        *, *:before, *:after { box-sizing: inherit; }
 
         input {
             width: 60%;
+        }
+
+        :host {
+            --white       : hsla(0, 0%, 100%, 1) ;
+            --transparent : hsla(0, 0%, 0%, 0);
+            --grey        : hsla(0, 0%, 90%, 1);
+            --blue        : hsla(207, 88%, 66%, 1);
+            position: relative;
+            width: 100%;
+            height: 16px;
+        }
+        input {
+            position: absolute;
+            top: 0;
+            left 0; 
+            width: 100%;
+            -webkit-appearance: none;
+            margin: 0;
+            z-index: 2;
+            background-color: var(--transparent); 
+        }
+        .bar {
+            position: absolute;
+            top: 3px;
+            left 0; 
+            z-index: 0;
+            height: 10px;
+            width: 100%;
+            border-radius: 8px;
+            overflow: hidden;
+            background-color: var(--grey); 
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+        .ruler {
+            position: absolute;
+            height: 6px;
+            width: 100%;
+            transform: scale(-1, 1);
+            background-size: 20px 8px;
+            background-image: repeating-linear-gradient(to right,
+                var(--grey) 0px,
+                var(--grey) 17px,
+                var(--white) 17px,
+                var(--white) 20px
+            ); 
+        }
+        .fill {
+            position: absolute;
+            height: 100%;
+            width: 80%;
+            background-color: var(--grey);
+        }
+        input:focus + .bar .fill,
+        input:focus-within + .bar .fill
+        input:active + .bar .fill {
+            background-color: var(--blue);
         }
     `
 }
